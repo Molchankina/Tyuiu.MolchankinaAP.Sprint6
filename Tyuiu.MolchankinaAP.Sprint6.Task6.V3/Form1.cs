6a4d052cc33e1586832a222d2ac3ddea709937bb
Molchankina <anqel892005@gmail.com>
2025-01-24 02:41:05 +0500

    Form1.cs

--- Tyuiu.MolchankinaAP.Sprint6.Task6.V3/Form1.cs
+++ Tyuiu.MolchankinaAP.Sprint6.Task6.V3/Form1.cs
@@ -1,3 +1,4 @@
+using Tyuiu.MolchankinaAP.Sprint6.Task6.V3.Lib;
 namespace Tyuiu.MolchankinaAP.Sprint6.Task6.V3
 {
     public partial class Form1 : Form
@@ -5,6 +6,34 @@ namespace Tyuiu.MolchankinaAP.Sprint6.Task6.V3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        string openFilePath;
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialogTask.ShowDialog();
+            openFilePath = openFileDialogTask.FileName;
+            textBoxInput.Text = File.ReadAllText(openFilePath);
+            groupBoxOutput.Text = groupBoxOutput.Text + " " + openFileDialogTask.FileName;
+            buttonDone.Enabled = true;
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = ds.CollectTextFromFile(openFilePath);
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            FormAbout formAbout = new FormAbout();
+            formAbout.ShowDialog();
+        }
+
+        private void textBoxTask_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
