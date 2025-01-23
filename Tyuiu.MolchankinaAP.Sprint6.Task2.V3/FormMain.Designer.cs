88042c9fb1675a73efbd39b043709e0c9a62c45f
Ангелина Молчанкина <molchankinaap@outlook.com>
2025-01-24 02:36:10 +0500

    Task2

--- Tyuiu.MolchankinaAP.Sprint6.Task2.V3/FormMain.Designer.cs
+++ Tyuiu.MolchankinaAP.Sprint6.Task2.V3/FormMain.Designer.cs
@@ -28,153 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1_MAP = new GroupBox();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label1 = new Label();
             groupBox1 = new GroupBox();
-            groupBox2_MAP = new GroupBox();
-            groupBox4_MAP = new GroupBox();
-            textBox2_MAP = new TextBox();
-            groupBox3_MAP = new GroupBox();
-            textBox1_MAP = new TextBox();
-            button1_MAP = new Button();
-            button2_MAP = new Button();
-            groupBoxKVK_MAP = new GroupBox();
-            groupBox1_MAP.SuspendLayout();
-            groupBox2_MAP.SuspendLayout();
-            groupBox4_MAP.SuspendLayout();
-            groupBox3_MAP.SuspendLayout();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            X = new DataGridViewTextBoxColumn();
+            F = new DataGridViewTextBoxColumn();
+            label3 = new Label();
+            groupBox3.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             SuspendLayout();
             // 
-            // groupBox1_MAP
+            // button1
             // 
-            groupBox1_MAP.Controls.Add(groupBox1);
-            groupBox1_MAP.Location = new Point(13, 15);
-            groupBox1_MAP.Name = "groupBox1_MAP";
-            groupBox1_MAP.Size = new Size(562, 302);
-            groupBox1_MAP.TabIndex = 0;
-            groupBox1_MAP.TabStop = false;
-            groupBox1_MAP.Text = "Условие";
+            button1.Location = new Point(76, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 48);
+            button1.TabIndex = 6;
+            button1.Text = "Выполнить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox2);
+            groupBox3.Controls.Add(textBox1);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label1);
+            groupBox3.Location = new Point(16, 265);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(299, 105);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Ввод данных";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(140, 72);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(8, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(140, 49);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Конец";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Старт";
             // 
             // groupBox1
             // 
-            groupBox1.Location = new Point(568, 0);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(250, 122);
+            groupBox1.Size = new Size(303, 232);
             groupBox1.TabIndex = 4;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Условие";
             // 
-            // groupBox2_MAP
+            // label2
             // 
-            groupBox2_MAP.Controls.Add(groupBox4_MAP);
-            groupBox2_MAP.Controls.Add(groupBox3_MAP);
-            groupBox2_MAP.Location = new Point(12, 323);
-            groupBox2_MAP.Name = "groupBox2_MAP";
-            groupBox2_MAP.Size = new Size(345, 104);
-            groupBox2_MAP.TabIndex = 1;
-            groupBox2_MAP.TabStop = false;
-            groupBox2_MAP.Text = "Ввод данных";
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 41);
+            label2.Name = "label2";
+            label2.Size = new Size(209, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Результат вывести в таблице";
             // 
-            // groupBox4_MAP
+            // groupBox2
             // 
-            groupBox4_MAP.Controls.Add(textBox2_MAP);
-            groupBox4_MAP.Location = new Point(172, 20);
-            groupBox4_MAP.Name = "groupBox4_MAP";
-            groupBox4_MAP.Size = new Size(167, 78);
-            groupBox4_MAP.TabIndex = 1;
-            groupBox4_MAP.TabStop = false;
-            groupBox4_MAP.Text = "Конец шага";
+            groupBox2.Controls.Add(chart1);
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Location = new Point(321, 18);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(640, 406);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Вывод данных";
             // 
-            // textBox2_MAP
+            // dataGridView1
             // 
-            textBox2_MAP.Location = new Point(6, 42);
-            textBox2_MAP.Name = "textBox2_MAP";
-            textBox2_MAP.Size = new Size(155, 27);
-            textBox2_MAP.TabIndex = 0;
-            textBox2_MAP.Text = "5";
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { X, F });
+            dataGridView1.Location = new Point(6, 58);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(153, 324);
+            dataGridView1.TabIndex = 1;
             // 
-            // groupBox3_MAP
+            // X
             // 
-            groupBox3_MAP.Controls.Add(textBox1_MAP);
-            groupBox3_MAP.Location = new Point(6, 20);
-            groupBox3_MAP.Name = "groupBox3_MAP";
-            groupBox3_MAP.Size = new Size(159, 78);
-            groupBox3_MAP.TabIndex = 0;
-            groupBox3_MAP.TabStop = false;
-            groupBox3_MAP.Text = "Старт шага";
+            X.HeaderText = "X";
+            X.MinimumWidth = 6;
+            X.Name = "X";
+            X.Width = 50;
             // 
-            // textBox1_MAP
+            // F
             // 
-            textBox1_MAP.Location = new Point(7, 42);
-            textBox1_MAP.Name = "textBox1_MAP";
-            textBox1_MAP.Size = new Size(145, 27);
-            textBox1_MAP.TabIndex = 0;
-            textBox1_MAP.Text = "-5";
-            textBox1_MAP.TextChanged += textBox1_TextChanged;
+            F.HeaderText = "F";
+            F.MinimumWidth = 6;
+            F.Name = "F";
+            F.Width = 50;
             // 
-            // button1_MAP
+            // label3
             // 
-            button1_MAP.BackColor = Color.DeepSkyBlue;
-            button1_MAP.Location = new Point(363, 343);
-            button1_MAP.Name = "button1_MAP";
-            button1_MAP.Size = new Size(94, 78);
-            button1_MAP.TabIndex = 2;
-            button1_MAP.Text = "Справка";
-            button1_MAP.UseVisualStyleBackColor = false;
-            // 
-            // button2_MAP
-            // 
-            button2_MAP.BackColor = Color.Green;
-            button2_MAP.Location = new Point(460, 343);
-            button2_MAP.Name = "button2_MAP";
-            button2_MAP.Size = new Size(115, 78);
-            button2_MAP.TabIndex = 3;
-            button2_MAP.Text = "Выполнить";
-            button2_MAP.UseVisualStyleBackColor = false;
-            button2_MAP.Click += button2_MAP_Click;
-            // 
-            // groupBoxKVK_MAP
-            // 
-            groupBoxKVK_MAP.Location = new Point(580, 15);
-            groupBoxKVK_MAP.Name = "groupBoxKVK_MAP";
-            groupBoxKVK_MAP.Size = new Size(483, 418);
-            groupBoxKVK_MAP.TabIndex = 4;
-            groupBoxKVK_MAP.TabStop = false;
-            groupBoxKVK_MAP.Text = "Вывод данных";
-            groupBoxKVK_MAP.Enter += groupBoxKVK_MAP_Enter;
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Результат";
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1075, 445);
-            Controls.Add(groupBoxKVK_MAP);
-            Controls.Add(button2_MAP);
-            Controls.Add(button1_MAP);
-            Controls.Add(groupBox2_MAP);
-            Controls.Add(groupBox1_MAP);
+            ClientSize = new Size(973, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(button1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
             Name = "FormMain";
             Text = "Form1";
-            Load += Form1_Load;
-            groupBox1_MAP.ResumeLayout(false);
-            groupBox2_MAP.ResumeLayout(false);
-            groupBox4_MAP.ResumeLayout(false);
-            groupBox4_MAP.PerformLayout();
-            groupBox3_MAP.ResumeLayout(false);
-            groupBox3_MAP.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox groupBox1_MAP;
-        private GroupBox groupBox2_MAP;
-        private GroupBox groupBox4_MAP;
-        private GroupBox groupBox3_MAP;
-        private TextBox textBox1_MAP;
-        private TextBox textBox2_MAP;
-        private Button button1_MAP;
-        private Button button2_MAP;
+        private Button button1;
+        private GroupBox groupBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label1;
         private GroupBox groupBox1;
-        private GroupBox groupBoxKVK_MAP;
+        private Label label2;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private Label label3;
+        private DataGridViewTextBoxColumn X;
+        private DataGridViewTextBoxColumn F;
     }
 }
--- Tyuiu.MolchankinaAP.Sprint6.Task2.V3/FormMain.cs
+++ Tyuiu.MolchankinaAP.Sprint6.Task2.V3/FormMain.cs
@@ -1,4 +1,4 @@
-namespace Tyuiu.MolchankinaAP.Sprint6.Task2.V3
+﻿namespace Tyuiu.MolchankinaAP.Sprint6.Task2.V3
 {
     public partial class FormMain : Form
     {
@@ -6,35 +6,24 @@ namespace Tyuiu.MolchankinaAP.Sprint6.Task2.V3
         {
             InitializeComponent();
         }
+        DataService ds = new DataService();
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBoxKVK_MAP_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-
-        }
-
-        private void button2_MAP_Click(object sender, EventArgs e)
-        {
-
+            int startValue = Convert.ToInt32(textBox1.Text);
+            int stopValue = Convert.ToInt32(textBox2.Text);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] result = new double[len];
+            result = ds.GetMassFunction(startValue, stopValue);
+            this.chart1.Titles.Add("Ãðàôèê ôóíêöèè F(X)= sin(x) / x + 1,2 + cos(x) * 7x - 2");
+            this.chart1.ChartAreas[0].AxisX.Title = "Îñü X";
+            this.chart1.ChartAreas[0].AxisY.Title = "Îñü Y";
+            for (int i = 0; i <= len - 1; i++)
+            {
+                this.dataGridView1.Rows.Add(Convert.ToString(startValue), Convert.ToString(result[i]));
+                this.chart1.Series[0].Points.AddXY(startValue, result[i]);
+                startValue++;
+            }
         }
     }
 }
