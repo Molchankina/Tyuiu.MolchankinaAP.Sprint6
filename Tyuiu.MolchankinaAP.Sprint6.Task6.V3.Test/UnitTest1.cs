33d60035934bf47970eb90643c9d3af49dfd0372
Molchankina <anqel892005@gmail.com>
2025-01-24 02:46:17 +0500

    DataServiceTest.cs

--- Tyuiu.MolchankinaAP.Sprint6.Task6.V3.Test/UnitTest1.cs
+++ Tyuiu.MolchankinaAP.Sprint6.Task6.V3.Test/UnitTest1.cs
@@ -1,11 +1,41 @@
+using Tyuiu.Molchankina.Sprint6.Task6.V3.Lib;
 namespace Tyuiu.MolchankinaAP.Sprint6.Task6.V3.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CollectTextFromFile_ValidFile_ReturnsWordsWithR()
         {
+            // Arrange
+            var service = new DataService();
+            var testFilePath = "testFile.txt";
+            File.WriteAllText(testFilePath, "apple orange rabbit rocket");
+
+            // Act
+            var result = service.CollectTextFromFile(testFilePath);
+
+            // Assert
+            Assert.AreEqual("orange rabbit rocket", result);
+
+            // Cleanup
+            File.Delete(testFilePath);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void CollectTextFromFile_NonExistentFile_ThrowsException()
+        {
+            var service = new DataService();
+            service.CollectTextFromFile("nonexistent.txt");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CollectTextFromFile_NullPath_ThrowsException()
+        {
+            var service = new DataService();
+            service.CollectTextFromFile(null);
         }
     }
 }
